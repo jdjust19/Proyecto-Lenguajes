643d1e616e20c548f8f8eea4a654ecf563fe750a
slapazromero <sergiolrlapaz@gmail.com>
2020-04-13 22:13:25 +0200

    Añadidas las imagenes para el primer nivel del juego, comienzo de la aplicacion javaScript, implementacion del cambio de pagina con los botones 	modified:   html/index.html 	new file:   img/casa.jpg 	new file:   img/edificio.jpg 	new file:   img/flores.jpg 	new file:   img/medusas.jpg 	new file:   "img/monta\303\261as.jpg" 	new file:   img/perros.jpg 	new file:   img/piedras.jpg 	new file:   img/pollos.jpg 	modified:   js/codigo.js

---
+++ js/codigo.js
@@ -0,0 +1,16 @@
+function ponerFotosAleatorias(){
+    var imagenArray = new Array("medusas.jpg",
+                                 "montañas.jpg",
+                                 "perros.jpg",
+                                 "piedras.jpg",
+                                 "pollos.jpg",
+                                 "flores.jpg",
+                                 "casa.jpg",
+                                 "edificio.jpg");
+    var numeroDeCeldas = ;
+    var elemento = document.body;
+    elemento.getElementsByTagName();
+    var imagenArrayLength = imagenArray.length;
+    var aleatorio = Math.round(Math.random * imagenArrayLength - 1);
+
+}
